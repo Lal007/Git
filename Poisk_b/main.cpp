e5719f30e8816f7c9db55f0fd0229554f714b628
Lal007 <42681868+Lal007@users.noreply.github.com>
2018-08-28 23:29:00 +0300

    5
    5

--- Poisk_b/main.cpp
+++ Poisk_b/main.cpp
@@ -14,12 +14,12 @@ int main()
 
    for (int i = 0; i  == int (s1.size()); i++)
    {
-        if (s1[i] == 'b' && word_new)
+        if (char (s1[i]) == "b" && word_new)
         {
           counter_b++;
           word_new = false;
            }
-        else if (s1[i] == '\0')
+        else if (s1[i] == 0)
           {
            word_new = true;
            debug++;
