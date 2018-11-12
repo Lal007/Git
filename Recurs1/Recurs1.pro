9f36d9ac857ad28dc90298dab01c3c52ae8f20b1
Lal007 <42681868+Lal007@users.noreply.github.com>
2018-11-12 16:56:28 +0300

    12.11.18

---
+++ Recurs1/Recurs1.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-11-12T15:14:17
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Recurs1
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Recurs1/main.cpp
@@ -0,0 +1,58 @@
+#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
+
+void Fak (int n, int arr[])
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i+1;
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        //if n =
+        cout << arr[i] << "  " ;
+    }
+    cout << endl;
+    int res = 1;
+    for (int i = 0; i < n; i++)
+    {
+
+        res *= arr[i];
+
+    }
+    cout << "Faktorial! = " << res << endl;
+}
+
+int Fuk (int n) {
+
+    if (n == 1 || n == 0){
+        return 1;
+    } else {
+
+        return n * Fuk(n-1);
+
+    }
+
+}
+
+int main()
+{
+    int n = 0;
+    cout << "Vvedite faktorial: ";
+    cin >> n;
+    if (n < 0){
+
+        cout << "Nedopustimo";
+    } else {
+        //int arr[n];
+        //Fak (n, arr);
+        cout << Fuk(n);
+    }
+
+    return 0;
+}
